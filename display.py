45cb0033a1a0af0a47b898360185e2aea074a194
Luis Dias <luisfiliperdias@gmail.com>
2016-05-02 23:33:59 +0100

    Implement first sketch of display class

--- display.py
+++ display.py
@@ -3,15 +3,42 @@
     Luis Filipe Ribeiro Dias
             2016
 """
+from Tkinter import *
+
+myFont = tkFont.Font(familit='Helvetica', size=36, weight='bold')
 
 
 class MDisplay(object):
     """
-        handles display related operations
+        Handles display related operations
     """
+
     def __init__(self):
         """
-            the initializer
+            the initializer sets up some display parameters
+        """
+        self.win = Tk()
+        # initialize window as black
+        self.win.configure(bg='#000000')
+        # give it a title
+        self.win.title("HSV Exercise")
+        # and a geometry
+        self.win.geometry('800x480')
+
+        # close button is always handy
+        exitButton = Button(self.win, text="Exit", font=myFont,
+                            command=exitProgram, height=2, width=6)
+        exitButton.pack(side=Bottom)
+
+    def update_bg(self, h, s, v):
+        """
+            update the background according to H, S & V
+        """
+        pass
+
+    def exitProgram(self):
+        """
+            called when exit button is pressed
         """
         pass
 
@@ -19,4 +46,4 @@ class MDisplay(object):
         """
             the terminator
         """
-        pass
+        self.win.quit()
