6b117d62c8eb85f050aa84e10610dc56d852c8a3
Luis Dias <luisfiliperdias@gmail.com>
2016-05-02 23:17:01 +0100

    Add skeleton for main and gpio/display classes

---
+++ display.py
@@ -0,0 +1,22 @@
+"""
+    This is the display class.
+    Luis Filipe Ribeiro Dias
+            2016
+"""
+
+
+class MDisplay(object):
+    """
+        handles display related operations
+    """
+    def __init__(self):
+        """
+            the initializer
+        """
+        pass
+
+    def __del__(self):
+        """
+            the terminator
+        """
+        pass
---
+++ gpio.py
@@ -0,0 +1,21 @@
+"""
+    This is the GPIO class.
+    Luis Filipe Ribeiro Dias
+            2016
+"""
+
+
+class MGPIO(object):
+    """
+        handles gpio related operations
+    """
+    def __init__(self):
+        """
+            the initializer
+        """
+
+    def __del__(self):
+        """
+            the terminator
+        """
+        pass
