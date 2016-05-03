6efa533cf9dac21c9f687254fdf0f8c2c4e32859
Luis Dias <luisfiliperdias@gmail.com>
2016-05-03 20:27:18 +0000

    Reduce weight of main

--- main.py
+++ main.py
@@ -8,8 +8,6 @@
 from gpio import MGPIO_H, MGPIO_S, MGPIO_V
 from display import MDisplay
 
-
-import colorsys
 import time
 
 def main():
@@ -17,23 +15,13 @@ def main():
         main function
     :return: 0 on success
     """
-    gpio_h = MGPIO_H()
-    gpio_s = MGPIO_S()
-    gpio_v = MGPIO_V()
 
     h_disp = MDisplay()
-#    h_disp.loop()
+    gpio_h = MGPIO_H(h_disp)
+    gpio_s = MGPIO_S(h_disp)
+    gpio_v = MGPIO_V(h_disp)
 
-    while True:
-        print("H: " + str(gpio_h.get_value()))
-        print("S: " + str(gpio_s.get_value()))
-        print("V: " + str(gpio_v.get_value()))
-
-   #    (r,g,b) = colorsys.hsv_to_rgb(gpio_h.get_value(), gpio_s.get_value(), gpio_v.get_value())
-        h_disp.update_bg(r, g ,b)
-
-        time.sleep(1)
-
+    h_disp.loop()
 
 if __name__ == '__main__':
     main()
