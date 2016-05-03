e690d5a18883e86f64697dc6aee12ad15abfdef5
Luis Filipe Dias <luis.dias@dtvresearch.com>
2016-05-03 10:12:40 +0100

    Refactor gpio, to make use of inheritance

--- gpio.py
+++ gpio.py
@@ -28,9 +28,20 @@ class MGPIO(object):
             the initializer
             GPIO's direction and event listeners are setup here
         """
-        self.colors = {'h': 0, 's': 0, 'v': 0}
 
         GPIO.setmode(GPIO.BCM)
+
+
+    def __del__(self):
+        """
+            the terminator
+        """
+        pass
+
+class MGPIO_H(MGPIO):
+
+    def __init__(self):
+        super().__init__()
 
         # GPIO's set up as inputs.
         # They should be pulled up to avoid false detections.
@@ -38,78 +49,90 @@ class MGPIO(object):
         # So we'll be setting up falling edge detection
         GPIO.setup(HUE_H, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(HUE_L, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Add event interrupt callbacks on falling edges
+        GPIO.add_event_detect(HUE_H, GPIO.FALLING,
+                              callback=self.inc_value, bouncetime=300)
+        GPIO.add_event_detect(HUE_L, GPIO.FALLING,
+                              callback=self.dec_value, bouncetime=300)
+        self.value = 0
+
+    def inc_value(self):
+        self.value += 1
+
+    def dec_value(self):
+        self.value -= 1
+
+    def get_value(self):
+        return self.value
+
+    def __del__(self):
+        """
+            the terminator
+        """
+        pass
+
+class MGPIO_S(MGPIO):
+
+    def __init__(self):
+        super().__init__()
+
+        # GPIO's set up as inputs.
+        # They should be pulled up to avoid false detections.
+        # Both ports are wired to connect to GND on button press.
+        # So we'll be setting up falling edge detection
         GPIO.setup(SATURATION_H, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(SATURATION_L, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Add event interrupt callbacks on falling edges
+        GPIO.add_event_detect(SATURATION_H, GPIO.FALLING,
+                              callback=self.inc_value, bouncetime=300)
+        GPIO.add_event_detect(SATURATION_L, GPIO.FALLING,
+                              callback=self.dec_value, bouncetime=300)
+        self.value = 0
+
+    def inc_value(self):
+        self.value += 1
+
+    def dec_value(self):
+        self.value -= 1
+
+    def get_value(self):
+        return self.value
+
+    def __del__(self):
+        """
+            the terminator
+        """
+        pass
+
+class MGPIO_V(MGPIO):
+
+    def __init__(self):
+        super().__init__()
+
+        # GPIO's set up as inputs.
+        # They should be pulled up to avoid false detections.
+        # Both ports are wired to connect to GND on button press.
+        # So we'll be setting up falling edge detection
         GPIO.setup(VALUE_H, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(VALUE_L, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # Add event interrupt callbacks on falling edges
-        GPIO.add_event_detect(HUE_H, GPIO.FALLING,
-                              callback=self.press_hue_h, bouncetime=300)
-        GPIO.add_event_detect(HUE_L, GPIO.FALLING,
-                              callback=self.press_hue_l, bouncetime=300)
-        GPIO.add_event_detect(SATURATION_H, GPIO.FALLING,
-                              callback=self.press_saturation_h, bouncetime=300)
-        GPIO.add_event_detect(SATURATION_L, GPIO.FALLING,
-                              callback=self.press_saturation_l, bouncetime=300)
         GPIO.add_event_detect(VALUE_H, GPIO.FALLING,
-                              callback=self.press_value_h, bouncetime=300)
+                              callback=self.inc_value, bouncetime=300)
         GPIO.add_event_detect(VALUE_L, GPIO.FALLING,
-                              callback=self.press_value_l, bouncetime=300)
+                              callback=self.dec_value, bouncetime=300)
+        self.value = 0
 
-    def press_hue_h(self):
-        """
-            more hue callback
-        """
-        self.colors['h'] += 1
+    def inc_value(self):
+        self.value += 1
 
-    def press_hue_l(self):
-        """
-            less hue callback
-        """
-        self.colors['h'] -= 1
-
-    def press_saturation_h(self):
-        """
-            more saturation callback
-        """
-        self.colors['s'] += 1
-
-    def press_saturation_l(self):
-        """
-            less saturation callback
-        """
-        self.colors['s'] -= 1
-
-    def press_value_h(self):
-        """
-            less value callback
-        """
-        self.colors['v'] += 1
-
-    def press_value_l(self):
-        """
-            less value callback
-        """
-        self.colors['v'] -= 1
-
-    def get_hue(self):
-        """
-            return the hue
-        """
-        return self.colors['h']
-
-    def get_saturation(self):
-        """
-            return the saturation
-        """
-        return self.colors['s']
+    def dec_value(self):
+        self.value -= 1
 
     def get_value(self):
-        """
-            return the value
-        """
-        return self.colors['v']
+        return self.value
 
     def __del__(self):
         """
