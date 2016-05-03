a7df807c164d0d3975aeeb0dd94dd168515b6039
Luis Dias <luisfiliperdias@gmail.com>
2016-05-03 19:10:41 +0000

    Change port numbers, based on developed button array

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from gpio import MGPIO_H, MGPIO_S, MGPIO_V
 from display import MDisplay
 
 
-import colorsys
-
+# import colorsys
+import time
 
 def main():
     """
@@ -20,11 +20,22 @@ def main():
     gpio_h = MGPIO_H()
     gpio_s = MGPIO_S()
     gpio_v = MGPIO_V()
-    h_disp = MDisplay()
 
-    (r,g,b) = colorsys.hsv_to_rgb(gpio_h.get_value(), gpio_s.get_value(), gpio_v.get_value())
-    h_disp.update_bg(r,g,b)
-    h_disp.loop()
+#    (r,g,b) = colorsys.hsv_to_rgb(gpio_h.get_value(), gpio_s.get_value(), gpio_v.get_value())
+#    h_disp.update_bg(r,g,b)
+#    h_disp = MDisplay()
+#    h_disp.loop()
+
+    while True:
+        print("H: " + str(gpio_h.get_value()))
+        print("S: " + str(gpio_s.get_value()))
+        print("S: " + str(gpio_v.get_value()))
+        # colorsys.hsv_to_rgb(h, s, v)
+        # h_disp.update_bg()
+
+        time.sleep(1)
+        # get h, s and v
+        # define the steps of each (10?)
 
 
 if __name__ == '__main__':
